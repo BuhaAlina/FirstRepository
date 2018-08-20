e99f7944e1f5f82635e4d5bf5030093919d170cb
“freecodecampalina” <alina.solcanu82@gmail.com>
2018-08-20 21:14:58 +0300

    user: delete update resetPasword createAdmin getUserByEmayl

--- Test1/src/main/java/com/test/Test/controller/UserController.java
+++ Test1/src/main/java/com/test/Test/controller/UserController.java
@@ -2,8 +2,11 @@ package com.test.Test.controller;
 
 import com.test.Test.helper.InternshipResponse;
 import com.test.Test.helper.ResponseObject;
+
 import com.test.Test.helper.VerifyEmail;
+import com.test.Test.model.Role;
 import com.test.Test.model.User;
+import com.test.Test.repository.RoleRepository;
 import com.test.Test.service.UserService;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,14 +16,17 @@ import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.web.bind.annotation.*;
 
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 @RestController
 public class UserController {
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+
 
 
 
@@ -61,7 +67,6 @@ public class UserController {
     //logout user --/logout*********
     @RequestMapping(value = "/logout", method = RequestMethod.POST)
     public ResponseEntity logout(@RequestHeader("reset_token") final String token) {
-
         User dbUser = userService.findUserByResetToken(token);
         if( dbUser != null){
             dbUser.setResetToken(null);
@@ -76,18 +81,94 @@ public class UserController {
     //get all users --/users*********
     @RequestMapping(value = "/users", method = RequestMethod.GET)
     public ResponseEntity users (@RequestHeader ("reset_token") final String token) {
-        User dbUser = userService.findUserByResetToken(token);
-        if (dbUser != null) {
+        //User dbUser = userService.findUserByResetToken(token);
+        //if(dbUser!= null)
+        if (userService.tokenIsValid(token)){
            List<ResponseObject> listUsers= new ArrayList<ResponseObject>();
            listUsers.addAll(userService.findAllUser());
             return ResponseEntity.status(HttpStatus.OK).body(new InternshipResponse(true, "List of users: ", listUsers));
         }
             return ResponseEntity.status(HttpStatus.OK).body(new InternshipResponse(false, "Access Denied", null));}
 
-    //create Admin --/create/admin*********
-    //PUT-- /create/admin--create admin account to admin
-    //user delete
-    //POST /reset -password
-    //PUT /user--update user
     //GET /user -get one user by email
+    @RequestMapping(value = "/user", method = RequestMethod.GET)
+    public ResponseEntity user (@RequestParam("email") final String email, @RequestHeader ("reset_token") final String token) {
+        if (userService.tokenIsValid(token)) {
+         User user =userService.findUserByEmail(email);
+            if (user != null) {
+                return ResponseEntity.status(HttpStatus.OK).body(new InternshipResponse(true, "Success", Arrays.asList(user)));
+            } else {return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new InternshipResponse(false, "Please provide a valid id.", null));}
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(new InternshipResponse(false, "You are not authorized to perform this operation!", null));
+    }
+
+    //PUT --/create/admin --create Admin account by admin *********
+    @RequestMapping(value="/create/admin",method = RequestMethod.PUT)
+    public ResponseEntity createAdmin(@RequestHeader("reset_token") final String token,@RequestBody User admin) {
+
+        User userAdmin = userService.findUserByResetToken(token);
+        if (userService. isAdmin(userAdmin)) {
+            String email = admin.getEmail();
+            if (VerifyEmail.emailIsValid(email)) {
+                if (userService.findUserByEmail(email) == null) {
+                    admin.setPassword(userService.hashPassword(admin.getPassword()));
+                    Role userRole = roleRepository.findByName("ADMIN");
+                    admin.setRoles(new HashSet<>(Collections.singletonList(userRole)));
+                    userService.saveUser(admin);
+                    return ResponseEntity.status(HttpStatus.OK).body(new InternshipResponse(true, "Admin [" + admin.getEmail() + "] created successfully!", Arrays.asList(admin)));
+
+                } else {
+                    return ResponseEntity.status(HttpStatus.OK).body(new InternshipResponse(false, "The selected email already belongs to an account. Please use a different one!", null)); }
+
+                } else {
+                    return ResponseEntity.status(HttpStatus.OK).body(new InternshipResponse(false, "The supplied email address is not valid!", null)); }
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(new InternshipResponse(false, "You are not authorized to perform this operation!", null));
+    }
+
+    //POST /reset -password
+    @RequestMapping(value = "/reset", method = RequestMethod.POST)
+    public ResponseEntity resetPassword (@RequestBody User user) {
+      User dbUser =userService.findUserByEmail(user.getEmail());
+      if(dbUser!=null) {
+          dbUser.setPassword(userService.hashPassword(user.getPassword()));
+          userService.saveUser(dbUser);
+          return ResponseEntity.status(HttpStatus.OK).body(new InternshipResponse(true, "Password Changed!", Arrays.asList(dbUser)));
+      } else {
+          return ResponseEntity.status(HttpStatus.OK).body(new InternshipResponse(false, "The provided email address doesn't belong to any existing account", null));
+      }
+    }
+    //PUT /user--update user
+    @RequestMapping(value = "/user", method = RequestMethod.PUT)
+    public ResponseEntity userUpdate (@RequestHeader("reset_token") final String token,@RequestBody User user) {
+        User dbUser = userService.findUserByResetToken(token);
+        if( dbUser != null){
+            dbUser.setLastName(user.getLastName());
+            dbUser.setFirstName(user.getFirstName());
+            dbUser.setEmail(user.getEmail());
+            userService.saveUser(dbUser);
+            return ResponseEntity.status(HttpStatus.OK).body(new InternshipResponse(true, "Successfull update!", Arrays.asList(dbUser)));
+        }else
+        {
+            return ResponseEntity.status(HttpStatus.OK).body(new InternshipResponse(false, "No session token available!", null));}
+
+
+    }
+    //DELETE --/delete --user soft-delete
+    @RequestMapping(value = "/user", method = RequestMethod.DELETE)
+    public ResponseEntity userDelete (@RequestHeader("reset_token") final String token,@RequestBody User user) {
+    User userAdmin = userService.findUserByResetToken(token);
+        if (userService. isAdmin(userAdmin)) {
+            User dbUser = userService.findUserByEmail(user.getEmail());
+            if(dbUser!=null){
+                dbUser.setActive(0);
+                dbUser.setResetToken(null);
+                userService.saveUser(dbUser);
+                return ResponseEntity.status(HttpStatus.OK).body(new InternshipResponse(true, "User  was deleted successfully!", Arrays.asList(dbUser)));
+            }else{ return ResponseEntity.status(HttpStatus.OK).body(new InternshipResponse(false, "The provided email doesn't belong to any existing user account.", null));}
+
+        } return ResponseEntity.status(HttpStatus.OK).body(new InternshipResponse(false, "You are not authorized to perform this operation!", null));
+    }
+
+
 }
--- Test1/src/main/java/com/test/Test/helper/VerifyEmail.java
+++ Test1/src/main/java/com/test/Test/helper/VerifyEmail.java
@@ -5,13 +5,14 @@ import java.util.regex.Pattern;
 
 public class VerifyEmail {
 
-    public static final Pattern VALID_EMAIL_ADDRESS_REGEX =
-            Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
-
-    public static boolean emailIsValid(String email){
-        Matcher matcher =VALID_EMAIL_ADDRESS_REGEX.matcher(email);
-        return matcher.find();
-    }
+        public static final Pattern VALID_EMAIL_ADDRESS_REGEX =
+                Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
 
 
+
+        public static boolean emailIsValid(String email){
+            Matcher matcher =VALID_EMAIL_ADDRESS_REGEX.matcher(email);
+            return matcher.find();
+
+        }
 }
